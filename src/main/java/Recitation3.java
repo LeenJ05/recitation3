7c202f9264bddebe9536913552720b179e2b30a4
Leen <leena@Leens-MacBook-Air.local>
2024-02-23 23:06:29 -0500

    Completed

--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -5,6 +5,22 @@ public class Recitation3 {
     {
         Scanner scan = new Scanner(System.in);
 
-        // add your code here
+        System.out.println("Enter x: ");
+        double x = scan.nextDouble();
+        System.out.println("Enter y: ");
+        double y = scan.nextDouble();
+
+        Point location = new Point(x, y);
+        Circle aCircle = new Circle(location, 16.0);
+
+        Circle bCircle = new Circle(new Point(14,12),40.0);
+
+        if (aCircle.intersects(bCircle)){
+            System.out.println("Circles intersect!");
+        } else {
+            System.out.println("Circles do not intersect!");
+        }
+
+
     }
 }
